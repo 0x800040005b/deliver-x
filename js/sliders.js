4f3575e9515b58f9dd266f18d7d4091697903e28
Vasyl Klishch <klishchvas@gmail.com>
2023-11-29 13:13:41 +0200

    finished adaptive

--- js/sliders.js
+++ js/sliders.js
@@ -4,34 +4,48 @@ import {Swiper} from 'swiper';
 import {Autoplay} from "swiper/modules";
 // import Swiper styles
 import 'swiper/css';
-document.addEventListener('DOMContentLoaded',function (){
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const direction = 'vertical',
+          slidesPerView = 3,
+          modules = [Autoplay],
+          speed = 5000;
+
     const sliderLeftOptions = {
         spaceBetween: 30,
-        speed: 5000,
-        slidesPerView: 3,
-        direction: 'vertical',
+        speed: 6000,
+        slidesPerView: slidesPerView,
+        direction: direction,
         loop: true,
         autoplay: {
-            enabled: true,
             delay: 1,
             disableOnInteraction: false,
         },
-        modules: [Autoplay],
-    }
-    const sliderRightOptions = {
-        spaceBetween: 30,
-        speed: 5000,
-        slidesPerView: 3,
-        direction: 'vertical',
-        loop: true,
-        autoplay: {
-            enabled: true,
-            delay: 1,
-            reverseDirection: true,
-            disableOnInteraction: false,
+        modules: modules,
+    },
+        sliderRightOptions = {
+            spaceBetween: 30,
+            speed: 5000,
+            slidesPerView: slidesPerView,
+            direction: direction,
 
-        },
-        modules: [Autoplay],
+            loop: true,
+            breakpoints: {
+                576: {
+                    autoplay: {
+                        delay: 1,
+                        disableOnInteraction: false,
+                    },
+
+                }
+            },
+            autoplay: {
+                delay: 1,
+                reverseDirection: false,
+                disableOnInteraction: false,
+            },
+            modules: modules,
     }
     const sliderLeft = new Swiper('.swiper__left', sliderLeftOptions);
     const sliderRight = new Swiper('.swiper__right', sliderRightOptions);
